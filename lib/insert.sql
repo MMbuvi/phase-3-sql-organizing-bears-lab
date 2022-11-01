c73a646353fa52638df40807dbcb6aec6e50dba9
Meshack <mbuvi.meshack99@gmail.com>
2022-11-01 15:44:02 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Mr. Chocolate", 5 ,"M", "Brown" , "Grumpy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Rowdy", 6 ,"F", "Black" , "Happy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Tabitha", 5 ,"F", "Brown" , "Grumpy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Sergeant Brown", 3 ,"M", "Happy" , "Grumpy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Melissa", 5 ,"F", "Blue" , "Clumsy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Grinch", 5 ,"M", "Yellow" , "Sad" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Wendy", 7 ,"F", "Green" , "Happy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ( NULL,6 ,"M", "Brown" , "Melencholic" , true);
